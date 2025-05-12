3164bc47a8fd9e82c17155cef93c2684925fd0a0
lrpick8 <lpickrell1@my.bcit.ca>
2025-05-12 14:17:09 -0700

    changed the port variable to be dynamic instead of statically 3000

--- server.js
+++ server.js
@@ -216,6 +216,7 @@ app.use((req, res) => {
 });
 
 // Start Server
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
